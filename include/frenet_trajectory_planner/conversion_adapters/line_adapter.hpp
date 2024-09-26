f1d2354e97d06493a887c897e7b5e580ecbd25b5
CihatAltiparmak <cihataltiparmak1@gmail.com>
2024-09-26 13:16:45 +0300

    Implemented frenet2cartesian convertions and moved test builds to separate cmake file

---
+++ include/frenet_trajectory_planner/conversion_adapters/line_adapter.hpp
@@ -0,0 +1,65 @@
+// #include <frenet_trajectory_planner/conversion_adapters/base_adapter.hpp>
+#pragma once
+#include <frenet_trajectory_planner/type_definitions.hpp>
+#include <cmath>
+
+#include <Eigen/Dense>
+#include <cmath>
+
+using namespace Eigen;
+
+namespace frenet_trajectory_planner
+{
+
+class LineAdapter
+{
+public:
+  LineAdapter(const Vector2d & t_frenet, const Vector2d & x0);
+  CartesianState convert_frenet2cartesian(const FrenetState & frenet_state);
+  FrenetState convert_cartesian2frenet(const CartesianState & cartesian_state);
+
+private:
+  Vector2d t_frenet_;
+  Vector2d x0_;
+};
+
+LineAdapter::LineAdapter(const Vector2d & t_frenet, const Vector2d & x0)
+: t_frenet_(t_frenet), x0_(x0)
+{
+}
+
+CartesianState LineAdapter::convert_frenet2cartesian(const FrenetState & frenet_state)
+{
+  CartesianState cartesian_state = CartesianState::Zero();
+
+  Matrix<double, 2, 2> T;
+  T << t_frenet_[0], -t_frenet_[1],
+    t_frenet_[1], t_frenet_[0];
+
+  cartesian_state({0, 3}) = T * frenet_state({0, 3}) + x0_;
+  cartesian_state({1, 4}) = T * frenet_state({1, 4});
+  cartesian_state({2, 5}) = T * frenet_state({2, 5});
+  cartesian_state(6) = std::atan2(t_frenet_[1], t_frenet_[0]) + std::atan2(
+    frenet_state[1],
+    frenet_state[4]);
+
+  return cartesian_state;
+
+}
+
+FrenetState LineAdapter::convert_cartesian2frenet(const CartesianState & cartesian_state)
+{
+  Matrix<double, 6, 2> T;
+  T << (cartesian_state[0] - x0_[0]), cartesian_state[3] - x0_[1],
+    cartesian_state[1], cartesian_state[4],
+    cartesian_state[2], cartesian_state[5],
+    -(cartesian_state[0] - x0_[0]), cartesian_state[3] - x0_[1],
+    -cartesian_state[1], cartesian_state[4],
+    -cartesian_state[2], cartesian_state[5];
+
+  FrenetState frenet_state = T * t_frenet_;
+
+  return frenet_state;
+}
+
+}
---
+++ test/conversion_adapters/line_adapter_test.cpp
@@ -0,0 +1,71 @@
+#include <vector>
+#include <tuple>
+#include <gtest/gtest.h>
+#include <frenet_trajectory_planner/conversion_adapters/line_adapter.hpp>
+
+#define _USE_MATH_DEFINES
+
+TEST(frenet_trajectory_planner, conversion_adapters_line_adapter_test_initialization) {
+  Vector2d t_frenet;
+  t_frenet << 0, 1;
+
+  Vector2d x0;
+  x0 << 1, 1;
+  auto adapter = frenet_trajectory_planner::LineAdapter(t_frenet, x0);
+}
+
+TEST(frenet_trajectory_planner, conversion_adapters_line_adapter_test_convert_frenet2cartesian) {
+  {
+    Vector2d t_frenet;
+    t_frenet << 0, 1;
+
+    Vector2d x0;
+    x0 << 1, 1;
+    auto adapter = frenet_trajectory_planner::LineAdapter(t_frenet, x0);
+
+    frenet_trajectory_planner::FrenetState frenet_state =
+      frenet_trajectory_planner::FrenetState::Zero();
+    frenet_trajectory_planner::CartesianState cartesian_state = adapter.convert_frenet2cartesian(
+      frenet_state);
+
+    ASSERT_NEAR(cartesian_state[6], M_PI / 2, 1e-10);
+  }
+
+  {
+    Vector2d t_frenet;
+    t_frenet << 1 / std::sqrt(2), 1 / std::sqrt(2);
+
+    Vector2d x0;
+    x0 << 1, 1;
+    auto adapter = frenet_trajectory_planner::LineAdapter(t_frenet, x0);
+
+    frenet_trajectory_planner::FrenetState frenet_state =
+      frenet_trajectory_planner::FrenetState::Zero();
+    frenet_trajectory_planner::CartesianState cartesian_state = adapter.convert_frenet2cartesian(
+      frenet_state);
+
+    ASSERT_NEAR(cartesian_state[6], M_PI / 4, 1e-10);
+  }
+
+  {
+    Vector2d t_frenet;
+    t_frenet << 1, 0;
+
+    Vector2d x0;
+    x0 << 1, 1;
+    auto adapter = frenet_trajectory_planner::LineAdapter(t_frenet, x0);
+
+    frenet_trajectory_planner::FrenetState frenet_state =
+      frenet_trajectory_planner::FrenetState::Zero();
+    frenet_trajectory_planner::CartesianState cartesian_state = adapter.convert_frenet2cartesian(
+      frenet_state);
+
+    ASSERT_NEAR(cartesian_state[6], 0, 1e-10);
+  }
+}
+
+int main(int argc, char ** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
