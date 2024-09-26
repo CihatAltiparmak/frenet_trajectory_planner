b902745e2080573d1cb4a2e4f4d3423873b61862
CihatAltiparmak <cihataltiparmak1@gmail.com>
2024-09-26 20:50:24 +0300

    Added test cases for cartesian to frenet state conversion

--- test/conversion_adapters/line_adapter_test.cpp
+++ test/conversion_adapters/line_adapter_test.cpp
@@ -64,6 +64,61 @@ TEST(frenet_trajectory_planner, conversion_adapters_line_adapter_test_convert_fr
   }
 }
 
+TEST(frenet_trajectory_planner, conversion_adapters_line_adapter_test_convert_cartesian2frenet) {
+  {
+    Vector2d t_frenet;
+    t_frenet << 0, 1;
+
+    Vector2d x0;
+    x0 << 1, 1;
+    auto adapter = frenet_trajectory_planner::LineAdapter(t_frenet, x0);
+
+    frenet_trajectory_planner::CartesianState cartesian_state;
+    cartesian_state << -1, 0, 0, 1, 0, 0, M_PI / 2;
+
+    frenet_trajectory_planner::FrenetState frenet_state = adapter.convert_cartesian2frenet(
+      cartesian_state);
+
+    ASSERT_NEAR(frenet_state[3], -2, 1e-10);
+  }
+
+  {
+    Vector2d t_frenet;
+    t_frenet << 0, 1;
+
+    Vector2d x0;
+    x0 << 1, 1;
+    auto adapter = frenet_trajectory_planner::LineAdapter(t_frenet, x0);
+
+    frenet_trajectory_planner::CartesianState cartesian_state;
+    cartesian_state << -1, 0, 0, 2, 0, 0, M_PI / 2;
+
+    frenet_trajectory_planner::FrenetState frenet_state = adapter.convert_cartesian2frenet(
+      cartesian_state);
+
+    ASSERT_NEAR(frenet_state[3], -2, 1e-10);
+    ASSERT_NEAR(frenet_state[0], 1, 1e-10);
+  }
+
+  {
+    Vector2d t_frenet;
+    t_frenet << 1 / std::sqrt(2), 1 / std::sqrt(2);
+
+    Vector2d x0;
+    x0 << 1, 1;
+    auto adapter = frenet_trajectory_planner::LineAdapter(t_frenet, x0);
+
+    frenet_trajectory_planner::CartesianState cartesian_state;
+    cartesian_state << -1, 0, 0, 3, 0, 0, M_PI / 2;
+
+    frenet_trajectory_planner::FrenetState frenet_state = adapter.convert_cartesian2frenet(
+      cartesian_state);
+
+    ASSERT_NEAR(frenet_state[3], -2 * std::sqrt(2), 1e-10);
+    ASSERT_NEAR(frenet_state[0], 0, 1e-10);
+  }
+}
+
 int main(int argc, char ** argv)
 {
   testing::InitGoogleTest(&argc, argv);
