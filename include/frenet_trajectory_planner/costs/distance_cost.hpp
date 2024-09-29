c07dc59ae0d16d400bb9b05af9f7e668e2abf628
Cihat Kurtuluş Altıparmak <cihataltiparmak1@gmail.com>
2024-09-30 00:38:44 +0300

    Implemented selection of the best trajectory (#3)
    * Started to implement best frenet trajectory selection mechanism and made
    this implementation integrable to various templated policies
    
    * Implemented frenet best trajectory selector and refactored some classes
    like policy classes

---
+++ include/frenet_trajectory_planner/costs/distance_cost.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <frenet_trajectory_planner/costs/distance_cost.hpp>
+#include <cmath>
+
+namespace frenet_trajectory_planner
+{
+namespace costs
+{
+
+class LateralDistanceCost : public Cost
+{
+public:
+  LateralDistanceCost(const double & K_distance);
+  double cost(const FrenetTrajectory & frenet_trajectory) override;
+
+private:
+  double K_distance_;
+};
+
+LateralDistanceCost::LateralDistanceCost(const double & K_distance)
+: Cost(), K_distance_(K_distance)
+{
+}
+
+double LateralDistanceCost::cost(const FrenetTrajectory & frenet_trajectory)
+{
+  double trajectory_cost = 0;
+
+  for (auto frenet_state : frenet_trajectory) {
+    trajectory_cost += std::abs(frenet_state[3] * K_distance_);
+  }
+
+  return trajectory_cost;
+}
+
+}
+}
--- include/frenet_trajectory_planner/policies/base_policy.hpp
+++ include/frenet_trajectory_planner/policies/base_policy.hpp
@@ -3,24 +3,38 @@
 
 #include <vector>
 #include <iostream>
+#include <frenet_trajectory_planner/type_definitions.hpp>
+
 
 namespace frenet_trajectory_planner
 {
 namespace policies
 {
 
-template<typename FrenetTrajectory, typename Parameters>
-class BasePolicy
+class Policy
+{
+public:
+  virtual std::vector<FrenetTrajectory> eliminate_frenet_trajectories(
+    const std::vector<FrenetTrajectory> & frenet_trajectory_array)
+  = 0;
+
+  virtual std::vector<CartesianTrajectory> eliminate_cartesian_trajectories(
+    const std::vector<CartesianTrajectory> & cartesian_trajectory_array)
+  = 0;
+
+  virtual bool check_frenet_trajectory_by_policy(const FrenetTrajectory & frenet_trajectory) = 0;
+  virtual bool check_cartesian_trajectory_by_policy(
+    const CartesianTrajectory & cartesian_trajectory) = 0;
+};
+
+template<typename Parameters>
+class BasePolicy : public Policy
 {
 public:
   BasePolicy(const Parameters & parameters)
   : parameters_(parameters)
   {
   }
-
-  virtual std::vector<FrenetTrajectory> eliminate_trajectories(
-    const std::vector<FrenetTrajectory> & frenet_trajectory_array)
-  = 0;
 
   Parameters getParameters() const
   {
